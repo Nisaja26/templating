6c7a3a00531ce101117b484da430ee4eecf85309
Nisaja26 <nissaikaracmawati@gmail.com>
2024-10-17 09:29:59 +0700

    pt 06 95%

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\AdminUsercontroller;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('template', function () {
+//     return view('template');
+// });
+
+
+// Route::get('/', function () {
+//     return view('admin.layouts.wrapper');
+// });
+
+
+Route::get('/', function () {
+    $data = [
+     'content' => 'admin.dashboard.index'
+    ];
+    return view('admin.layouts.wrapper', $data);
+ });
+
+Route::prefix('/admin')->group(function () {
+    Route::resource('/user', AdminUsercontroller::class);
+});
+
+
+
+
+// Route::get('/post', function () {
+//     $data = [
+//         'content' => 'admin.post.index' 
+//     ];
+//     return view('admin.layouts.wrapper', $data);
+// });
+
+
+
